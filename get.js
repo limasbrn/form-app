cce549a717cd66cd92f46e3fe3402d7944aceadd
Sabrina Silva <lima.sbrn@gmail.com>
2022-06-29 14:34:40 -0300

    login authentication

---
+++ get.js
@@ -0,0 +1,31 @@
+
+async function getUsers() {
+  let response = await fetch("http://localhost:3000/register");
+  let data = await response.json();
+  let userName = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+  let alert = document.getElementById("alert_success");
+
+  const findUser = data.find(user => user.username.includes(userName) && user.password.includes(pass));
+  
+   function get(){
+
+    if (findUser === undefined){
+      alert.style.color = "#9c0e04"
+      alert.innerHTML="Check your information, something is Wrong !"
+
+    }else if(findUser){
+      alert.style.color = "#10725b"
+      alert.innerHTML="WELCOME!"
+
+    }else if((userName === '') || (pass === '')){
+      alert.style.color = "#9c0e04"
+      alert.innerHTML="You left some information out!" 
+    }
+  };
+   
+  get()
+
+}
+
+ 
--- post.js
+++ post.js
@@ -1,20 +1,19 @@
-function fazPost(url, body){
-       console.log(body)
-}
+
 
 function cadastraUsuario(){
     event.preventDefault()
-    let url = "http://localhost:3000/register"
-    let userName = document.getElementById('username').value
-    let email = document.getElementById('email').value
-    let pass =document.getElementById('password').value
-    let pass2 =document.getElementById('password2').value
-    let message = document.getElementById('alert_success')
+    let url = "http://localhost:3000/register";
+    let userName = document.getElementById('username').value;
+    let email = document.getElementById('email').value;
+    let pass =document.getElementById('password').value;
+    let pass2 =document.getElementById('password2').value;
+    let message = document.getElementById('alert_success');
+    let body = document.getElementById('form');
    
-    body = {
-        "username":userName,
-        "email":email,
-        "password":pass
+   body = {
+        "username": userName,
+        "email": email,
+        "password": pass
     }
     
     if((pass === pass2) && (userName != '') && (email != '')) {
@@ -25,5 +24,17 @@ function cadastraUsuario(){
         message.style.color= "#9c0e04"
         message.innerHTML="Check your information, something is wrong!"
     }
+
+
+    function fazPost(url, body){
+        console.log(body)
+        let request = new XMLHttpRequest()
+        request.open("POST", url, true)
+        request.setRequestHeader("Content-type", "application/json")
+        request.send(JSON.stringify(body))
+ 
+       
+ 
+ }
     
 }
